d440cff7783db4791cfe11dabe5a32182e201e4b
pvhoang <pvhoang@tma.com.vn>
2018-11-02 17:33:44 +0700

    11/2 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var notes = require('../models/notes-memory');
+router.get('/', function (req, res, next) {
+  notes.keylist()
+    .then(keylist => {
+      var keyPromises = [];
+      for (var key of keylist) {
+        keyPromises.push(
+          notes.read(key)
+            .then(note => {
+              return { key: note.key, title: note.title };
+            })
+        );
+      }
+      return Promise.all(keyPromises);
+    })
+    .then(notelist => {
+      res.render('index', { title: 'Notes', notelist: notelist });
+    })
+    .catch(err => { next(err); });
 });
-
 module.exports = router;
